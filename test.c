eb427542213ec22316a456eba8a20b0af1a5c550
Aysuarex <aysuarex@gmail.com>
2022-06-19 02:18:46 +0100

    Added runing man ASCII Art

--- test.c
+++ test.c
@@ -40,7 +40,7 @@ void main()
     "   aaaaaaaaaaaaaa   aaaa8aaa8aaa8aaa8aaa8aaaa   88   \n"
     "   88                   <                       88   \n"
     "   88aaaa   aaaaaaaaaaaaaaaaa   aaaaaaaaaaaaa   88   \n"
-    "   888888   88888888888888888 | 8888888888888 A 88   \n"
+    "   888888   88888888888888888 | 8888888888888 X 88   \n"
     "   88aaa8aaa8aaa8aaa8aaa8aaaa   aaaa8aaa8aaa8aaa88   \n"
     "   88888888888888                 <        8888888   \n"
     "   88aaa8aaa8aaa8aaaa   aaaaaaaaaaaaa   aaaa8aaa88   \n"
@@ -63,7 +63,7 @@ void main()
     MAZE3:
     printf(
     "   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa   \n"
-    "   88                               88888888888888   \n"
+    "   88                             X 88888888888888   \n"
     "   88aaaa   aaaaaaaaa   aaaaa   aaaa8aaa8aaa8aaa88   \n"
     "   888888   888888888   88888   88888           88   \n"
     "   88aaaa   aaaa8aaa8aaa8aaa8aaa8aaa8aaaa   aaaa88   \n"
@@ -73,7 +73,7 @@ void main()
     "   88aaaa   aaaa8aaa8aaaaaaaaaaaa   aaaaa   8aaa88   \n"
     "  >     >        888888888888               888888   \n"
     "   aaaaaa   aaaa8aaa8aaa8aaa8aaaaaaaaaaaa   aaaa88   \n"
-    "   888888 | 888888888888888888888     >       >   >  \n"
+    "   888888 | 888888888888888888888     >     >     >  \n"
     "   88aaaa   aaaa8aaa8aaa8aaa8aaa8aaaa   aaaaaaaaaa   \n"
     "   88           >           888888888 | 8888888888   \n"
     "   88aaaa   aaaaaaaaa   aaaa8aaa8aaaa   aaaa8aaa88   \n"
@@ -83,6 +83,32 @@ void main()
     "   88aaa8aaaa   aaaaa   aaaaa   aaaaa   aaaa8aaa88   \n"
     "   888888                 >         >       888888   \n"
     "   88aaa8aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa8aaa88   \n"
+    );
+    */
+    /*
+    MAZE4:
+    printf(
+    "   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa   \n"
+    "   888888                                       88   \n"
+    "   88aaa8aaa8   8aaa8aaa8aaa8   8aaa8aaa8   8aaa88   \n"
+    "   88   88888   88888   88888   888888888   888888   \n"
+    "   88   8aaa8   8aaa8   8aaa8aaa8aaa8aaa8   8aaa88   \n"
+    "   88           88888       <       88888     X 88   \n"
+    "   88   8aaa8   8aaa8   8aaa8   8aaa8aaa8   8aaa88   \n"
+    "   88   88888   88888 | 88888 | 888888888   888888   \n"
+    "   88aaa8aaa8   8aaa8   8aaa8   8aaa8aaa8aaa8aaa88   \n"
+    "   888888         <        88    <       <      88   \n"
+    "   88aaa8aaa8aaa8   8aaa8aaa8   8aaa8aaa8   8aaa88   \n"
+    "   888888   88888 | 888888888   888888888   888888   \n"
+    "   88aaa8   8aaa8   8aaa8aaa8aaa8aaa8aaa8   8aaa88   \n"
+    "   888888      <        888888888   88888 |     88   \n"
+    "   88aaa8   8aaa8aaa8aaa8aaa8aaaa   8aaa8   8aaa88   \n"
+    "   88     | 88888888888888888       88888       88   \n"
+    "   88aaa8   8aaa8aaa8aaa8aaa8aaaa   aaaaa   8aaa88   \n"
+    "   888888 | 888888888                     | 888888   \n"
+    "   88aaa8   8aaa8aaa8aaa8   8aaa8aaa8aaa8   8aaa88   \n"
+    "  <     <      888888888   8aaa8aaa8      <     <    \n"
+    "   aaaaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaaaa   \n"
     );
     */
     /*
@@ -112,28 +138,28 @@ void main()
     printf("\n\t\t---------------------------------------------------------------------------\n");
 */
 /*
-    printf("\t\t\taaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa   a\n");
-    printf("\t\t\t8   8               8               8           8                   8   8\n");
-    printf("\t\t\t8   8   aaaaaaaaa   8   aaaaa   aaaa8aaaa   aaaa8   aaaaa   aaaaa   8   8\n");
-    printf("\t\t\t8               8       8   8           8           8   8   8       8   8\n");
-    printf("\t\t\t8aaaaaaaa   a   8aaaaaaa8   8aaaaaaaa   8aaaa   a   8   8   8aaaaaaa8   8\n");
-    printf("\t\t\t8       8   8               8           8   8   8   8   8           8   8\n");
-    printf("\t\t\t8   a   8aaa8aaaaaaaa   a   8   aaaaaaaa8   8aaa8   8   8aaaaaaaa   8   8\n");
-    printf("\t\t\t8   8               8   8   8       8           8           8       8   8\n");
-    printf("\t\t\t8   8aaaaaaaaaaaa   8aaa8   8aaaa   8   aaaaa   8aaaaaaaa   8   aaaa8   8\n");
-    printf("\t\t\t8           8       8   8       8   8       8           8   8           8\n");
-    printf("\t\t\t8   aaaaa   8aaaa   8   8aaaa   8   8aaaaaaa8   a   a   8   8aaaaaaaaaaa8\n");
-    printf("\t\t\t8       8       8   8   8       8       8       8   8   8       8       8\n");
+    printf("\t\t\taaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa | a\n");
+    printf("\t\t\t8   8       <       8 <      <      8           8      <            8   8\n");
+    printf("\t\t\t8   8 | aaaaaaaaa | 8   aaaaa   aaaa8aaaa   aaaa8 | aaaaa   aaaaa   8 | 8\n");
+    printf("\t\t\t8        >      8     < 8   8  <      < 8           8   8 | 8       8   8\n");
+    printf("\t\t\t8aaaaaaaa   a |  8aaaaaaa8   8aaaaaaaa   8aaaa   a   8   8   8aaaaaaa8   8\n");
+    printf("\t\t\t8       8   8      >     >  8       >   8   8   8   8   8     <     8   8\n");
+    printf("\t\t\t8   a   8aaa8aaaaaaaa   a   8 | aaaaaaaa8   8aaa8 | 8   8aaaaaaaa | 8 | 8\n");
+    printf("\t\t\t8   8               8   8 | 8     < 8           8      >    8    >  8   8\n");
+    printf("\t\t\t8   8aaaaaaaaaaaa   8aaa8   8aaaa   8   aaaaa   8aaaaaaaa   8 | aaaa8   8\n");
+    printf("\t\t\t8           8       8   8  >    8   8       8     <     8 | 8   <    <  8\n");
+    printf("\t\t\t8   aaaaa   8aaaa   8   8aaaa   8 | 8aaaaaaa8 | a   a | 8   8aaaaaaaaaaa8\n");
+    printf("\t\t\t8       8       8   8   8       8     < 8  <    8   8   8   >   8       8\n");
     printf("\t\t\t8aaaaaaa8aaaa   8   8   8   aaaa8aaaa   8   aaaa8   8   8aaaa   8aaaa   8\n");
-    printf("\t\t\t8           8   8           8       8   8       8   8       8           8\n");
-    printf("\t\t\t8   aaaaa   8   8aaaaaaaa   8aaaa   8   8aaaa   8aaa8   aaaa8aaaaaaaa   8\n");
-    printf("\t\t\t8   8       8           8           8       8   8   8               8   8\n");
-    printf("\t\t\t8   8   aaaa8aaaa   a   8aaaa   aaaa8aaaa   8   8   8aaaaaaaaaaaa   8   8\n");
-    printf("\t\t\t8   8           8   8   8   8   8           8               8   8       8\n");
+    printf("\t\t\t8           8   8           8       8   8  >    8   8 |     8  >     >   8\n");
+    printf("\t\t\t8   aaaaa   8   8aaaaaaaa   8aaaa   8 | 8aaaa   8aaa8   aaaa8aaaaaaaa   8\n");
+    printf("\t\t\t8   8       8           8           8     < 8 | 8   8   <     <     8 | 8\n");
+    printf("\t\t\t8   8   aaaa8aaaa   a   8aaaa   aaaa8aaaa   8   8   8aaaaaaaaaaaa | 8   8\n");
+    printf("\t\t\t8   8           8   8   8   8   8  >     >  8  >      >     8   8    <  8\n");
     printf("\t\t\t8   8aaaaaaaa   8   8   8   8aaa8   8aaaaaaa8   aaaaaaaaa   8   8aaaaaaa8\n");
-    printf("\t\t\t8   8       8   8   8           8           8   8       8               8\n");
-    printf("\t\t\t8   8   aaaa8   8aaa8   aaaaa   8aaaaaaaa   8aaa8   a   8aaaaaaaa   a   8\n");
-    printf("\t\t\t8   8                   8           8               8               8   8\n");
+    printf("\t\t\t8   8       8   8   8           8  <     <  8   8  <    8    >          8\n");
+    printf("\t\t\t8   8   aaaa8   8aaa8   aaaaa   8aaaaaaaa   8aaa8   a | 8aaaaaaaa | a   8\n");
+    printf("\t\t\t8   8                   8           8       <     < 8        <      8   8\n");
     printf("\t\t\t8   8aaaaaaaaaaaaaaaaaaa8aaaaaaaaaaa8aaaaaaaaaaaaaaa8aaaaaaaaaaaaaaa8aaa8\n");
     */
     /*
@@ -143,32 +169,30 @@ void main()
     "8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8
     );
     */
-    
-    MAZE4:
+    /*
     printf(
-    "                                                   \n"
-    "   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa   \n"
-    "   88888                                       8   \n"
-    "   8aaa8aaa8   8aaa8aaa8aaa8   8aaa8aaa8   8aaa8   \n"
-    "   8   88888   88888   88888   888888888   88888   \n"
-    "   8   8aaa8   8aaa8   8aaa8aaa8aaa8aaa8   8aaa8   \n"
-    "   8           88888               88888     A 8   \n"
-    "   8   8aaa8   8aaa8   8aaa8   8aaa8aaa8   8aaa8   \n"
-    "   8   88888   88888   88888   888888888   88888   \n"
-    "   8aaa8aaa8   8aaa8   8aaa8   8aaa8aaa8aaa8aaa8   \n"
-    "   88888                   8                   8   \n"
-    "   8aaa8aaa8aaa8   8aaa8aaa8   8aaa8aaa8   8aaa8   \n"
-    "   88888   88888   888888888   888888888   88888   \n"
-    "   8aaa8   8aaa8   8aaa8aaa8aaa8aaa8aaa8   8aaa8   \n"
-    "   88888               888888888   88888       8   \n"
-    "   8aaa8   8aaa8aaa8aaa8aaa8aaaa   8aaa8   8aaa8   \n"
-    "   8       88888888888888888       88888       8   \n"
-    "   8aaa8   8aaa8aaa8aaa8aaa8aaaa   aaaaa   8aaa8   \n"
-    "   88888   888888888                       88888   \n"
-    "   8aaa8   8aaa8aaa8aaa8   8aaa8aaa8aaa8   8aaa8   \n"
-    "    <          888888888   8aaa8aaa8        <      \n"
-    "   8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8aaa8   \n"
-    "                                                   \n"
+    "\\                          /           \n"
+    " \\                        /            \n"
+    "  \\                      /             \n"
+    "  ]                     [    ,`|       \n"
+    "  ]                     [   /  |       \n"
+    "  ]___               ___[ ,'   |       \n"
+    "  ]  ]\\             /[  [ |:   |       \n"
+    "  ]  ] \\           / [  [ |:   |       \n"
+    "  ]  ]  ]         [  [  [ |:   |       \n"
+    "  ]  ]  ]__     __[  [  [ |:   |       \n"
+    "  ]  ]  ] ]\\ _ /[ [  [  [ |:   |       \n"
+    "  ]  ]  ] ] (#) [ [  [  [ :===='        \n"
+    "  ]  ]  ]_].nHn.[_[  [  [               \n"
+    "  ]  ]  ]  HHHHH. [  [  [               \n"
+    "  ]  ] /   `HH{'N  \\ [  [              \n"
+    "  ]__]/     HHH  \"  \\[__[             \n"
+    "  ]         NNN         [               \n"
+    "  ]         N/\"         [               \n"
+    "  ]         N H         [               \n"
+    "  /         N            \\             \n"
+    " /          q,            \\            \n"
+    "/                          \\        \n"
     );
-
+    */
 }
