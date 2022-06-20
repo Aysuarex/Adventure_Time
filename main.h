9ec4b60944e505196b798081140507d9699a2f58
Aysuarex <aysuarex@gmail.com>
2022-06-20 21:55:20 +0100

    Removed unecessary function prototypes

--- main.h
+++ main.h
@@ -1,11 +1,13 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+void main();
 void mallam();
 void kalakuta();
 void katanga();
 void baluba();
-int timer(int m);
+//int timer_and_map(int m);
+//void timer(int m);
 //int mapCount();
 
 
