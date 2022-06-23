a1135dd5a8839e913e934fdf22b17ded96fc4fbb
Aysuarex <aysuarex@gmail.com>
2022-06-23 23:47:18 +0100

    ASCII art representations for venues

---
+++ locations.c
@@ -0,0 +1,38 @@
+/**
+ * @file locations.c
+ * @author Suara Ayomide (aysuarex@gmail.com)
+ * 
+ * mallam_pic -
+ * baluba_pic -
+ * katanga_pic -
+ * kalakuta_pic - 
+ * 
+ * @date 2022-06-22
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+void mallam_pic()
+{
+
+    return;
+}
+
+void baluba_pic()
+{
+
+    return;
+}
+
+void katanga_pic()
+{
+
+    return;
+}
+
+void kalakuta_pic()
+{
+    
+    return;
+}
