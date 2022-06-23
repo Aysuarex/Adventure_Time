55eb1e51472946486d8a950ffada3cc2089d1ae6
Aysuarex <aysuarex@gmail.com>
2022-06-23 23:47:51 +0100

    ASCII art representation for the mazes

---
+++ maps.c
@@ -0,0 +1,134 @@
+/**
+ * @file maps.c
+ * @author Suara Ayomide (aysuarex@gmail.com)
+ * 
+ * mallam_map -
+ * kalakuta_map -
+ * katanga_map -
+ * baluba_map -
+ * 
+ * @date 2022-06-20
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+void mallam_map()
+{
+    printf(
+    "\n\t\t\t                                        START        \n"
+    "\t\t\t                                          |          \n"
+    "\t\t\t   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa   aaaaaa   \n"
+    "\t\t\t   888888   88888   8888888888888   88888   888888   \n"
+    "\t\t\t   88aaa8   8aaa8   8aaa8aaa8aaa8   8aaa8   8aaa88   \n"
+    "\t\t\t   88                                           88   \n"
+    "\t\t\t   88aaaa   aaaaa   aaaaa   aaaaa   aaaaa   aaaa88   \n"
+    "\t\t\t   888888   88888   88888   88888   88888   888888   \n"
+    "\t\t\t   88aaa8aaa8aaa8aaa8aaaa   aaaa8aaa8aaa8aaa8aaa88   \n"
+    "\t\t\t   88888888888888               888888888888888888   \n"
+    "\t\t\t   88aaa8aaa8aaa8aaaa   aaaaaaaaaaaa8aaa8aaa8aaa88   \n"
+    "\t\t\t   888888   888888888   88888   88888   8888888888   \n"
+    "\t\t\t   88aaa8   aaaa8aaaa   8aaa8   aaaaa   8aaa8aaa88   \n"
+    "\t\t\t   888888               88888           8888888888   \n"
+    "\t\t\t   88aaa8   aaaaaaaaa   8aaa8   aaaaa   aaaa8aaa88   \n"
+    "\t\t\t   888888   88888aaaa   88888   88888       888888   \n"
+    "\t\t\t   88aaaa   8aaa8aaaa   aaaa8aaa8aaaa   aaaaaaaa88   \n"
+    "\t\t\t   88       88888                       8888888888   \n"
+    "\t\t\t   88aaaa   8aaa8aaaa   aaaaaaaaaaaaa   8aaa8aaa88   \n"
+    "\t\t\t   888888   888888888   8888888888888   88888   88   \n"
+    "\t\t\t   88aaa8   aaaa8aaa8   8aaa8aaa8aaaa   aaaaa   88   \n"
+    "\t\t\t   888888       88888   888888888               88   \n"
+    "\t\t\t   88aaa8   aaaa8aaaa   aaaa8aaa8aaaaaaaaaaaa   88   \n"
+    "\t\t\t   888888   88888           88888888888888888   88   \n"
+    "\t\t\t   88aaa8aaa8aaa8aaaaaaaaaaa8aaa8aaa8aaa8aaa8   88   \n"
+    "\t\t\t                                              |      \n"
+    "\t\t\t                                             END     \n"
+    );
+    return;
+}
+
+void kalakuta_map()
+{
+    printf(
+    "\n\t\t\t   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa   \n"
+    "\t\t\t   88                               88888888888888   \n"
+    "\t\t\t   88aaaa   aaaaaaaaa   aaaaa   aaaa8aaa8aaa8aaa88   \n"
+    "\t\t\t   888888   888888888   88888   88888           88   \n"
+    "\t\t\t   88aaaa   aaaa8aaa8aaa8aaa8aaa8aaa8aaaa   aaaa88   \n"
+    "\t\t\t   88           888888888   88888   88888   888888   \n"
+    "\t\t\t   88aaaa   aaaa8aaa8aaaa   aaaaa   8aaa8   8aaa88   \n"
+    "\t\t\t   888888   888888888               88888   888888   \n"
+    "\t\t\t   88aaaa   aaaa8aaa8aaaaaaaaaaaa   aaaaa   8aaa88   \n"
+    "\t\t      START             8888888888888               888888   \n"
+    "\t\t\t   aaaaaa   aaaa8aaa8aaa8aaa8aaaaaaaaaaaa   aaaa88   \n"
+    "\t\t\t   888888   888888888888888888888                 END\n"
+    "\t\t\t   88aaaa   aaaa8aaa8aaa8aaa8aaa8aaaa   aaaaaaaaaa   \n"
+    "\t\t\t   88                       888888888   8888888888   \n"
+    "\t\t\t   88aaaa   aaaaaaaaa   aaaa8aaa8aaaa   aaaa8aaa88   \n"
+    "\t\t\t   888888   88888           88888           888888   \n"
+    "\t\t\t   88aaa8aaa8aaa8aaaa   aaaa8aaa8aaaa   aaaa8aaa88   \n"
+    "\t\t\t   8888888888   88888   88888   88888   8888888888   \n"
+    "\t\t\t   88aaa8aaaa   aaaaa   aaaaa   aaaaa   aaaa8aaa88   \n"
+    "\t\t\t   888888                                   888888   \n"
+    "\t\t\t   88aaa8aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa8aaa88   \n\n"
+    );
+    return;
+}
+
+void baluba_map()
+{
+    printf(
+    "\t\t\t   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa   \n"
+    "\t\t\t     END  <      <       <  8888888888888888888888888   88   \n"
+    "\t\t\t   aaaaaaaaaaaaaa   aaaa8aaa8aaa8aaa8aaa8aaaa   88   \n"
+    "\t\t\t   88                   <                       88   \n"
+    "\t\t\t   88aaaa   aaaaaaaaaaaaaaaaa   aaaaaaaaaaaaa   88   \n"
+    "\t\t\t   888888   88888888888888888 | 8888888888888 X 88   \n"
+    "\t\t\t   88aaa8aaa8aaa8aaa8aaa8aaaa   aaaa8aaa8aaa8aaa88   \n"
+    "\t\t\t   88888888888888                 <        8888888   \n"
+    "\t\t\t   88aaa8aaa8aaa8aaaa   aaaaaaaaaaaaa   aaaa8aaa88   \n"
+    "\t\t\t   88           88888   8888888888888 | 8888888888   \n"
+    "\t\t\t   88aaaa   aaaa8aaa8aaa8aaa8aaa8aaaa   aaaa8aaa88   \n"
+    "\t\t\t   888888       88888888888888888           888888   \n"
+    "\t\t\t   88aaaa   aaaaaaaaa   8aaa8aaa8aaaa   aaaa8aaa88   \n"
+    "\t\t\t   88           >       88888   88888 | 8888888888   \n"
+    "\t\t\t   88aaaa   aaaaaaaaa   aaaaa   aaaa8   aaaa8aaa88   \n"
+    "\t\t\t   888888 | 888888888     >     88888       888888   \n"
+    "\t\t\t   88aaaa   aaaa8aaa8   aaaaa   aaaaa   aaaa8aaa88   \n"
+    "\t\t\t   88           88888   88888 | 88888 | 8888888888   \n"
+    "\t\t\t   88aaaa | aaaa8aaa8aaa8aaaa   aaaaa   aaaa8aaa88   \n"
+    "\t\t\t   888888   8888888888888         >         888888   \n"
+    "\t\t\t   88aaa8   8aaa8aaa8aaa8aaaaaaaaaaaaaaaaaaa8aaa88   \n"
+    "\t\t\t          |                                          \n"
+    "\t\t\t        START                                        \n"
+    );
+    return;
+}
+
+void katanga_map()
+{
+    printf(
+    "\t\t\t   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa   \n"
+    "\t\t\t   888888                                       88   \n"
+    "\t\t\t   88aaa8aaaa   aaaaaaaaaaaaa   aaaaaaaaa   aaaa88   \n"
+    "\t\t\t   88   88888   88888   88888   888888888   888888   \n"
+    "\t\t\t   88   aaaaa   8aaa8   aaaa8aaa8aaa8aaa8   aaaa88   \n"
+    "\t\t\t   88           88888       <       88888     X 88   \n"
+    "\t\t\t   88   aaaaa   8aaa8   aaaaa   aaaa8aaa8   aaaa88   \n"
+    "\t\t\t   88   88888   88888 | 88888 | 888888888   888888   \n"
+    "\t\t\t   88aaa8aaaa   aaaaa   aaaa8   8aaa8aaa8aaa8aaa88   \n"
+    "\t\t\t   888888         <        88    <       <      88   \n"
+    "\t\t\t   88aaa8aaaaaaaa   aaaaaaaa8   aaaaaaaaa   aaaa88   \n"
+    "\t\t\t   888888   88888 | 888888888   888888888   888888   \n"
+    "\t\t\t   88aaa8   aaaaa   aaaa8aaa8aaa8aaa8aaa8   aaaa88   \n"
+    "\t\t\t   888888      <        888888888   88888 |     88   \n"
+    "\t\t\t   88aaaa   aaaaaaaaaaaa8aaa8aaaa   8aaa8   aaaa88   \n"
+    "\t\t\t   88     | 88888888888888888       88888       88   \n"
+    "\t\t\t   88aaaa   aaaa8aaa8aaa8aaa8aaaa   aaaaa   aaaa88   \n"
+    "\t\t\t   888888 | 888888888                     | 888888   \n"
+    "\t\t\t   88aaaa   aaaa8aaa8aaaa   aaaaaaaaaaaaa   aaaa88   \n"
+    "\t\t\t     END  <      <      888888888   8aaa8aaa8      <     <  START  \n"
+    "\t\t\t   aaaaaaaaaaaaaaaaaaaaa8aaa8aaa8aaa8aaaaaaaaaaaaa   \n"
+    );
+    return;
+}
