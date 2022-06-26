e5a476cd985296dabad960184bce4e240bc16941
Aysuarex <aysuarex@gmail.com>
2022-06-26 22:18:31 +0100

    Added story() prototypes to header file

--- main.h
+++ main.h
@@ -7,21 +7,25 @@ void mallam();
 void mallam_map();
 void mallam_pic();
 void mallam_treasure();
+void mallam_story();
 
 void kalakuta();
 void kalakuta_map();
 void kalakuta_pic();
 void kalakuta_treasure();
+void kalakuta_story();
 
 void katanga();
 void katanga_map();
 void katanga_pic();
 void katanga_treasure();
+void katanga_story();
 
 void baluba();
 void baluba_map();
 void baluba_pic();
 void baluba_treasure();
+void baluba_story();
 
 
 #endif
